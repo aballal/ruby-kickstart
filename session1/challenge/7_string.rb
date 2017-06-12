d10154876289bd480c403ffd6fb19610cb0d620a
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-12 16:20:32 +0100

    Completed 7_string

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,18 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  (1..string.length).map { |i| string[i] if string[i-1] == 'r' || string[i-1] == 'R'}.join
 end
+
+=begin
+puts pirates_say_arrrrrrrrr("")
+puts pirates_say_arrrrrrrrr("r")
+puts pirates_say_arrrrrrrrr("re")
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+=end
