59f0901384c54b604fefd9ac8c7275b8828b6d35
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-12 22:41:20 +0100

    Completed 4_array

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,15 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+def get_squares(numbers)
+  numbers.select {|num| num if numbers.include?(num ** 2) }.sort
+end
+
+=begin
+p get_squares [9]                      # => []
+p get_squares [9,3]                    # => [3]
+p get_squares [9,3,81]                 # => [3, 9]
+p get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
+p get_squares [3,9,3]                  # => [3, 3]
+=end
