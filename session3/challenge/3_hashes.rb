6919a3fefa91f6e8f0b4bdba3960b5264e380674
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-15 15:26:04 +0100

    session 3: 3_hashes

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,10 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+def word_count(string)
+  word_frequency = Hash.new(0)
+  string.downcase.split.each {|word| word_frequency[word] += 1}
+  word_frequency
+end
+
+#p word_count("The dog and the cat")
