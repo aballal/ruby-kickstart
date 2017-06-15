bf8a38b9d9977bc5acf078eb8489c769e35cd8e7
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-15 13:08:44 +0100

    Completed session 3: 1_blocks

--- session3/challenge/1_blocks.rb
+++ session3/challenge/1_blocks.rb
@@ -2,4 +2,15 @@
 # and returns an array of their results.
 #
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
+def reverse_map(*array)
+    array.reverse.map {|n| result = yield(n)}
+=begin
+    #Implementing without Ruby methods reverse and map
+    #Commented after rake 3:1 passed
+    results=[]
+    (array.length-1).downto(0) { |i| results << yield(array[i]) }
+    results
+=end
+end
 
+# reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
