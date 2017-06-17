aae3765e00ee8ee18024444a59d132d744f54d6c
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-18 00:30:19 +0100

    Completed session 3: 16_hash

--- session3/challenge/16_hash.rb
+++ session3/challenge/16_hash.rb
@@ -15,3 +15,26 @@
 # head = {:data => 6, :next => head}
 # print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
 
+def print_list (head)
+  #Never lose your head!
+  current = head
+  while current!= nil
+    print "#{current[:data]}\n"
+    current = current[:next]
+  end
+end
+
+=begin
+head = {:data => 1, :next => nil}
+print_list head                    # >> "1\n"
+head = {:data => 2, :next => head}
+print_list head                    # >> "2\n1\n"
+head = {:data => 3, :next => head}
+print_list head                    # >> "3\n2\n1\n"
+head = {:data => 4, :next => head}
+print_list head                    # >> "4\n3\n2\n1\n"
+head = {:data => 5, :next => head}
+print_list head                    # >> "5\n4\n3\n2\n1\n"
+head = {:data => 6, :next => head}
+print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
+=end
