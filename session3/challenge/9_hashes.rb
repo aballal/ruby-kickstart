b9aad80879855b91a13c1db3c3c3fe038e38fc71
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-17 08:51:36 +0100

    Completed session 3: 9_hashes

--- session3/challenge/9_hashes.rb
+++ session3/challenge/9_hashes.rb
@@ -29,4 +29,18 @@
 # shared [1,2,3], [3,2,1]            # => [{1=>[true, true], 2=>[true, true], 3=>[true, true]}, [1, 2, 3]]
 
 def shared(a, b)
+  hash_ab = Hash.new()
+  a.each {|e| hash_ab[e] = [true,nil]}
+  b.each {|e| hash_ab[e] ? hash_ab[e] = [true,true] : hash_ab[e] = [nil,true]}
+  [hash_ab,hash_ab.select{|key,value| value == [true,true]}.keys.to_a]
 end
+
+=begin
+p shared [1,2,3], [1,2,4]            # => [{1=>[true, true], 2=>[true, true], 3=>[true, nil], 4=>[nil, true]}, [1, 2]]
+p shared %w(a b c d), %w(aa b cc d)  # => [{"a"=>[true, nil], "b"=>[true, true], "c"=>[true, nil], "d"=>[true, true], "aa"=>[nil, true], "cc"=>[nil, true]}, ["b", "d"]]
+p shared [], [1,2]                   # => [{1=>[nil, true], 2=>[nil, true]}, []]
+p shared [1,2], []                   # => [{1=>[true, nil], 2=>[true, nil]}, []]
+p shared [], []                      # => [{}, []]
+p shared [1,2,:c], ['a','b',:c]      # => [{1=>[true, nil], 2=>[true, nil], :c=>[true, true], "a"=>[nil, true], "b"=>[nil, true]}, [:c]]
+p shared [1,2,3], [3,2,1]            # => [{1=>[true, true], 2=>[true, true], 3=>[true, true]}, [1, 2, 3]]
+=end
